0376829b995306fc15100448cb4957db3cbca35a
frostjaw <frostjaw>
2020-04-10 18:00:58 +0700

    0.0.1

---
+++ ios_lab2/LogInViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LogInViewController.swift
+//  ios_lab2
+//
+//  Created by Frostjaw on 09/04/2020.
+//  Copyright © 2020 Frostjaw. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+  
+  // MARK: - constants
+  
+  enum Constants {
+    static let containerViewCornerRadius = CGFloat(3)
+    static let buttonsCornerRadius = CGFloat(8)
+  }
+  
+  @IBOutlet weak var logInContainerView: UIView!
+  @IBOutlet weak var logInButton: UIButton!
+  @IBOutlet weak var signUpButton: UIButton!
+  
+  
+  @IBAction func signUpButtonTouchDown(_ sender: Any) {
+    let registerViewController = RegisterViewController()
+    self.present(registerViewController, animated: true, completion: nil)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    launchLogInContainerView()
+    launchButtons()
+    
+  }
+  
+  private func launchLogInContainerView(){
+    logInContainerView.layer.cornerRadius = Constants.containerViewCornerRadius
+  }
+  
+  private func launchButtons(){
+    logInButton.layer.cornerRadius = Constants.buttonsCornerRadius
+    signUpButton.layer.cornerRadius = Constants.buttonsCornerRadius
+  }
+  
+}
---
+++ ios_lab2/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  ios_lab2
+//
+//  Created by Frostjaw on 10/04/2020.
+//  Copyright © 2020 Frostjaw. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+}
